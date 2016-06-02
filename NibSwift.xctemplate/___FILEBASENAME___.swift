456f2bfe354e73594da9198c7edc9895bc8b230e
Richard Adem <richy486@gmail.com>
2016-06-02 10:03:29 -0400

    Added template files.

---
+++ NibSwift.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ___FILEBASENAMEASIDENTIFIER___ : UIView, NibLoader {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        addViewFromNib(withName: "___FILEBASENAMEASIDENTIFIER___")
+    }
+}
