93ac74c123ce4642fa8a8cdcffd3dc4e8f0236c6
Richard Adem <richy486@gmail.com>
2016-10-12 22:30:10 -0400

    Added programmatic screen template.
    Programmatic view controller and view template.

---
+++ OnesieScreen.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,45 @@
+//
+//  ___FILENAME___View
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAME___View: UIView {
+    
+    // MARK - Subviews lazy var
+    
+    internal var hasSetupConstraints = false
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Setup this view
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if !hasSetupConstraints {
+            hasSetupConstraints = true
+            
+            // Setup frames or constraints
+        }
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ OnesieScreen.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAME___ViewController: UIViewController {
+    
+    // MARK: - View
+    
+    var contentView:___FILEBASENAME___View {
+        get {
+            return self.view as! ___FILEBASENAME___View
+        }
+    }
+    
+    // MARK: - View lifecycle
+    
+    override func loadView() {
+        self.view = ___FILEBASENAME___View()
+        
+        // Relationships between view controller and view
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - Memory manager
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
