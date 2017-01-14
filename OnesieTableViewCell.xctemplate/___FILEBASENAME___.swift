dfec157e2ec65a43a2afd4f0320ab9f1cb649b37
Richard Adem <richy486@gmail.com>
2017-01-14 14:01:31 -0500

    Added Onesie TableViewCell template
    Based on the UIView template.

---
+++ OnesieTableViewCell.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,34 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAME___: UITableViewCell {
+    
+    // MARK: - Subviews lazy var, (add to contentView)
+    
+    // MARK: - View lifecycle
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // Setup this view
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Memory manager
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
