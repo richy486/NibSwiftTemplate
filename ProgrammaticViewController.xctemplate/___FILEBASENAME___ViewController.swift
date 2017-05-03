06c6979a4aeb177e484f2be348931f2df84882b6
Richard Adem <richy486@gmail.com>
2017-05-03 14:06:27 -0400

    Added Programmatic View Controller without a separate UIView subclass

---
+++ ProgrammaticViewController.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAME___ViewController: UIViewController {
+    
+    // MARK: - Subviews
+    
+    // MARK: - Properties   
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup Subviews
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Actions
+
+    // MARK: - Public methods
+
+    // MARK: - Private methods
+
+    // MARK: - Memory manager
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: - Extensions
